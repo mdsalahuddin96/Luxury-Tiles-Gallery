6e07cf66ca98b6b1f8d2bf5b36554fec62ebb676
mdsalauddin96 <salauddincse96@gmail.com>
2026-05-01 02:53:04 +0600

    Generate JSON data and and edit image link

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,27 @@
 import Banner from "@/components/Banner";
 import MarqueeText from "@/components/MarqueeText";
+import { getData } from "@/service/getData";
 import Image from "next/image";
+// import image from '@/assets/heroImage1.jpg'
 
-export default function Home() {
+export default async function Home() {
+  const tiles = await getData();
+  console.log(tiles[0]);
   return (
     <div>
-      <Banner/>
-      <MarqueeText/>
-      <h1>Hello</h1>
+      <Banner />
+      <MarqueeText />
+      <div className="grid grid-cols-4 gap-2">
+        {tiles.map((tile) => (
+          <Image
+            key={tile.id}
+            src={tile.image}
+            alt="tiles"
+            width={600}
+            height={600}
+          ></Image>
+        ))}
+      </div>
     </div>
   );
 }
